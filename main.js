a48bff98e177196c296f6329f996e5e3c2fb27ec
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2017-12-21 14:58:39 +0200

    class comments

--- main.js
+++ main.js
@@ -9,6 +9,39 @@ var nameInput = nameInput[0].toUpperCase() + nameInput.slice(1).toLowerCase();
 document.getElementById("yourname").innerHTML = nameInput
 */
 
+
+// we can use filter
+// reservation.filter(function(name){ toLowerCase})
+// search key in an object in js
+
+
+// class
+// 
+/*
+function findClient(clientName) {
+  for(var name in reservations) {
+    if(name.toLowerCase() == clientName.toLowerCase()) {
+      return reservations[Name];
+    }
+  }
+  return null;
+}
+
+
+var obj = findClient(clientName);
+if (obj == null) {
+  //add new reservations
+}
+else {
+  // say welcome or tell he it is claimed
+}
+*/
+
+///////////////////////////////////////////////////////////
+
+
+// my solution
+
 var claimReservation = function () {
   var nameInput = document.getElementById("nameInput").value;  
   var nameInput = nameInput[0].toUpperCase() + nameInput.slice(1).toLowerCase();
